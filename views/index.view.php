3143a1246b85c2c6b076d63a5cb196c24d68652d
david <davidsanchezdiaz72@gmail.com>
2024-02-03 06:56:54 +0100

    Mostar post en pagina principal (titulo, fecha,  extracto  e imagen)

--- views/index.view.php
+++ views/index.view.php
@@ -43,5 +43,37 @@
             </div>
         </div>
     </header>
+
+    <div class="contenedor">
+    <!-- Contenedor principal para el artículo -->
+    <div class="post">
+        <!-- Etiqueta de artículo -->
+        <article>
+            <!-- Título del artículo -->
+            <h2 class="titulo">Titulo del articulo</h2>
+            <!-- Fecha de publicación -->
+            <p class="fecha">1 Enero 2020</p>
+
+            <!-- Contenedor para la imagen destacada -->
+            <div class="thumb">
+                <!-- Enlace a la imagen (puede redirigir a la página completa del artículo) -->
+                <a href="#">
+                    <!-- Imagen destacada del artículo -->
+                    <img src="<?php echo RUTA; ?>/imagenes/1.png" alt="">
+                </a>
+            </div>
+
+            <!-- Extracto del artículo -->
+            <p class="extracto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, deleniti ipsam.
+                Voluptatum minima rerum dolorem earum, id voluptates veritatis consectetur temporibus.
+                Harum obcaecati tenetur dolorem, in cum enim quidem deserunt.
+            </p>
+
+            <!-- Enlace para continuar leyendo (puede redirigir a la página completa del artículo) -->
+            <a href="#" class="continuar">Continuar leyendo...</a>
+        </article>
+    </div>
+</div>
+
 </body>
 </html>
