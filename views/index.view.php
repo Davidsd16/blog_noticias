9bd2981cb3f66bec71682354c3d47235adc882a3
david <davidsanchezdiaz72@gmail.com>
2024-02-03 05:01:26 +0100

    Estilos css parte 2 (final). Elegir fuentes de las letras

--- views/index.view.php
+++ views/index.view.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bloc!, ultimas noticias!!</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/css/estilos.css">
+    <title>Blog Noticias</title>
 </head>
 <body>
-    
+    <header>
+        <div class="contenedor">
+            <div class="logo izquierda">
+                <p><a href="#">Ultimas Noticias</a></p>
+            </div>
+
+            <div class="derecha">
+                <p><a href="#">Ultimas Noticias</a></p>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
