2b6ecc2920410bca4e4068daf742d3f9c35ebb96
david <davidsanchezdiaz72@gmail.com>
2024-02-27 21:59:32 +0100

    Formulario de editar articulo, parte 1

--- views/editar.view.php
+++ views/editar.view.php
@@ -1,19 +1,32 @@
-<?php
-// Iniciamos la sesión
-session_start();
+<!-- Incluye el encabezado de la página -->
+<?php require 'header.view.php';?> 
 
-// Requerimos el archivo de configuración y el controlador
-require 'config.php';
-require '../controller/controller.php';
+<div class="contenedor">
+    <div class="post">
+        <article>
+			<!-- Título del formulario para editar un artículo -->
+            <h2 class="titulo">Editar Artículo</h2> 
+			<!-- Formulario para editar un artículo -->
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data" class="formulario" method="post">
+            
+                <!-- Campo oculto para almacenar el ID del artículo -->
+                <input type="hidden" value="" name="id">
+                <!-- Campo para ingresar el título del artículo -->
+                <input type="text" name="titulo" placeholder="Título del Artículo">
+				<!-- Campo para ingresar el extracto del artículo -->
+                <input type="text" name="extracto" placeholder="Extracto del Artículo">
+				<!-- Campo para ingresar el texto del artículo -->
+                <textarea name="texto" placeholder="Texto del Artículo"></textarea>
+				<!-- Campo para seleccionar una nueva imagen como miniatura del artículo -->
+                <input type="file" name="thumb">
 
-// Verificamos si la sesión está iniciada, si no, redirigimos al usuario
-comprobarSesion();
-
-// Conectamos a la base de datos
-$conexion = conexion($bd_config);
-if(!$conexion){
-    // Si hay un problema con la conexión, redirigimos a la página de error
-	header('Location: ../error.php');
-}
-
-require '../views/editar.view.php';
+                <!-- Campo oculto para almacenar la imagen actual del artículo -->
+                <input type="hidden" value="" name="thumb-guardada">
+				<!-- Botón para enviar el formulario y modificar el artículo --> 
+                <input type="submit" value="Modificar Artículo"> 
+            </form>
+        </article>
+    </div>
+</div>
+<!-- Incluye el pie de página de la página -->
+<?php require 'footer.php'; ?> 
